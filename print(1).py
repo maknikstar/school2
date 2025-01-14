2c229ca9f22a199a1b742f2fb8287c58f2396127
maknikstar <101141546+maknikstar@users.noreply.github.com>
2025-01-14 16:44:49 +0300

    Update print(1).py

--- print(1).py
+++ print(1).py
@@ -1,6 +1,5 @@
 print(1)
 print(2)
-status_otnosheniy_Matushi-ne_vstrechaetsia_s_Soney = True
-if status_otnosheniy_Matushi == True:
-  Matusha = "Lox"
+
+If 
 #https://thecode.media/face-train/ - статья с системой
